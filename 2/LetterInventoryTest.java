2e25d928b5c2a0101053e6d56c40d0b9438f9f0d
Rebecca Turner <637275@gmail.com>
2017-09-14 01:25:05 -0400

    tests

--- 2/LetterInventoryTest.java
+++ 2/LetterInventoryTest.java
@@ -7,9 +7,16 @@ public class LetterInventoryTest {
 	@Test
 	public void percentTest() {
 		LetterInventory a = new LetterInventory("abc");
-		assertEquals(
-			0.33333333333333,
-			a.getLetterPercentage('a'),
-			DELTA);
+		assertEquals(0.33333333333333,
+			a.getLetterPercentage('a'), DELTA);
+		assertEquals(0.33333333333333,
+			a.getLetterPercentage('b'), DELTA);
+		a = new LetterInventory("Hillary Clinton");
+		assertEquals(0.142857,
+			a.getLetterPercentage('n'), DELTA);
+		assertEquals(0.214286,
+			a.getLetterPercentage('l'), DELTA);
+		assertEquals(0.071429,
+			a.getLetterPercentage('y'), DELTA);
 	}
 }
