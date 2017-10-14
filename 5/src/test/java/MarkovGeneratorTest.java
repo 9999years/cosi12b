dc427d38ad6c8dc253e882342e7d0978f29dbffd
Rebecca Turner <637275@gmail.com>
2017-10-13 20:29:46 -0400

    working

--- 5/src/test/java/MarkovGeneratorTest.java
+++ 5/src/test/java/MarkovGeneratorTest.java
@@ -5,18 +5,27 @@ import org.junit.jupiter.api.Test;
 
 public class MarkovGeneratorTest {
 	void matchTest(Integer[] arr, int inx1, int inx2, boolean expected) {
-		assertEquals(expected, (new MarkovGenerator<Integer>(arr, 3))
-			.matches(inx1, inx2),
+		MarkovGenerator m = new MarkovGenerator<Integer>(arr, 3);
+		assertEquals(expected, m.matches(inx1, inx2),
 			"ensuring "
-			+ arr
+			+ m
 			+ " matches at "
 			+ inx1 + " and " + inx2);
 	}
 
 	@Test
 	void matchTest() {
-		matchTest(new Integer[] {1, 2, 3, 1, 2, 3}, 2, 5, true);
-		matchTest(new Integer[] {1, 2, 3, 1, 2, 3}, 0, 3, true);
-		matchTest(new Integer[] {1, 2, 3, 1, 2, 3}, 1, 4, true);
+		Integer[] a = new Integer[] {1, 2, 3, 1, 2, 3};
+		matchTest(a, 2, 5, true);
+		matchTest(a, 0, 3, true);
+		matchTest(a, 1, 4, true);
+		matchTest(a, 5, 2, true);
+		matchTest(a, 3, 0, true);
+		matchTest(a, 4, 1, true);
+		matchTest(a, 0, 0, true);
+		matchTest(a, 5, 5, true);
+
+		// <0 inx
+		matchTest(a, 5, -1, false);
 	}
 }
