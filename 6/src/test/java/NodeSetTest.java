b7b34e90855d09180f9a56792023b295accb3a9c
Rebecca Turner <637275@gmail.com>
2017-10-19 17:52:24 -0400

    unrunning test

--- 6/src/test/java/NodeSetTest.java
+++ 6/src/test/java/NodeSetTest.java
@@ -29,42 +29,41 @@ public class NodeSetTest {
 		}
 	}
 
-	@Test
-	static void integration() {
+	static NodeSet constructSet(int[][] dat) {
 		NodeSetFactory factory = new NodeSetFactory();
 
-		int[][] datA = new int[][] {
-			{3, 2, 0, 1}, // 0
-			{1, 0, 2, 3}, // 1
-			{0, 2, 3, 1}, // 2
-			{3, 2, 0, 1}  // 3
-		};
-
-		for(int i = 0; i < datA.length; i++) {
+		for(int i = 0; i < dat.length; i++) {
 			factory.add(i);
-			factory.addPrefs(datA[i]);
+			factory.addPrefs(dat[i]);
 		}
 
-		NodeSet setA = factory.getSet();
-		factory.reset();
+		return factory.getSet();
+	}
 
-		int[][] datB = new int[][] {
-			{3, 2, 4, 1},
-			{2, 3, 1, 4},
-			{3, 1, 2, 4},
-			{3, 2, 4, 1}
-		};
+	static void verifySets(
+			int[][] datA,
+			int[][] datB,
+			int[][] matches) {
+		NodeSet setA = constructSet(datA);
+		NodeSet.match(setA, constructSet(datB));
+		assertSetMatches(setA, matches);
+	}
 
-		for(int i = 0; i < datB.length; i++) {
-			factory.add(i);
-			factory.addPrefs(datB[i]);
-		}
-
-		NodeSet setB = factory.getSet();
-
-		NodeSet.match(setA, setB);
-
-		assertSetMatches(setA,
+	@Test
+	static void integration() {
+		verifySets(
+			new int[][] {
+				{3, 2, 0, 1}, // 0
+				{1, 0, 2, 3}, // 1
+				{0, 2, 3, 1}, // 2
+				{3, 2, 0, 1}  // 3
+			},
+			new int[][] {
+				{3, 2, 4, 1},
+				{2, 3, 1, 4},
+				{3, 1, 2, 4},
+				{3, 2, 4, 1}
+			},
 			new int[][] {
 				{0, 2},
 				{1, 1},
@@ -74,28 +73,33 @@ public class NodeSetTest {
 			}
 		);
 
-		//1, 3, 4, 2, 1, 6, 7, 5
-		//2, 6, 4, 2, 3, 5, 1, 7
-		//3, 6, 3, 5, 7, 2, 4, 1
-		//4, 1, 6, 3, 2, 4, 7, 5
-		//5, 1, 6, 5, 3, 4, 7, 2
-		//6, 1, 7, 3, 4, 5, 6, 2
-		//7, 5, 6, 2, 4, 3, 7, 1
-
-		//1, 4, 5, 3, 7, 2, 6, 1
-		//2, 5, 6, 4, 7, 3, 2, 1
-		//3, 1, 6, 5, 4, 3, 7, 2
-		//4, 3, 5, 6, 7, 2, 4, 1
-		//5, 1, 7, 6, 4, 3, 5, 2
-		//6, 6, 3, 7, 5, 2, 4, 1
-		//7, 1, 7, 4, 2, 6, 5, 3
-
-
-		//2, 5
-		//3, 1
-		//4, 3
-		//5, 7
-		//6, 6
-		//7, 2
+		verifySets(
+			new int[][] {
+				{1, 3, 4, 2, 1, 6, 7, 5},
+				{2, 6, 4, 2, 3, 5, 1, 7},
+				{3, 6, 3, 5, 7, 2, 4, 1},
+				{4, 1, 6, 3, 2, 4, 7, 5},
+				{5, 1, 6, 5, 3, 4, 7, 2},
+				{6, 1, 7, 3, 4, 5, 6, 2},
+				{7, 5, 6, 2, 4, 3, 7, 1}
+			},
+			new int[][] {
+				{1, 4, 5, 3, 7, 2, 6, 1},
+				{2, 5, 6, 4, 7, 3, 2, 1},
+				{3, 1, 6, 5, 4, 3, 7, 2},
+				{4, 3, 5, 6, 7, 2, 4, 1},
+				{5, 1, 7, 6, 4, 3, 5, 2},
+				{6, 6, 3, 7, 5, 2, 4, 1},
+				{7, 1, 7, 4, 2, 6, 5, 3}
+			},
+			new int[][] {
+				{2, 5},
+				{3, 1},
+				{4, 3},
+				{5, 7},
+				{6, 6},
+				{7, 2}
+			}
+		);
 	}
 }
